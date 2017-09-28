c7de6685fea0f428383d912a0698a0ec531e375d
Jorge Bianquetti <jbianquetti@bitnami.com>
2017-09-28 15:47:00 +0200

    fix deployment filename

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 
 case ${1} in
   create)
-     kubectl create -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bamboo/deploy.yaml
+     kubectl create -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bamboo/deployment.yaml
      kubectl create -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bamboo/svc.yml
-     kubectl create -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bitbucket/deploy.yaml
+     kubectl create -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bitbucket/deployment.yaml
      kubectl create -f  https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bitbucket/svc.yml
      ;;
   delete)
-     kubectl delete -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bamboo/deploy.yaml
+     kubectl delete -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bamboo/deployment.yaml
      kubectl delete -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bamboo/svc.yml
-     kubectl delete -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bitbucket/deploy.yaml
+     kubectl delete -f https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bitbucket/deployment.yaml
      kubectl delete -f  https://raw.githubusercontent.com/jbianquetti-nami/atlassian-k8s/master/bitbucket/svc.yml
      ;;
    *)
